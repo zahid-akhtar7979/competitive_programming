55ad9f650556498517de349e077267cb40975fb9
zahid-akhtar7979 <73689151+zahid-akhtar7979@users.noreply.github.com>
2021-08-16 15:53:09 +0530

    refactored code

--- string_manipulation/src/com/java/strings/PlusOne.java
+++ string_manipulation/src/com/java/strings/PlusOne.java
@@ -6,7 +6,9 @@ import java.util.Arrays;
 public class PlusOne {
 	
 	public static void main(String[] args) {
+		
 		ArrayList<Integer>list = new ArrayList<Integer>(Arrays.asList(9,9,9,9,9));
+		
 		System.out.println(plusOne(list));
 	}
 	
